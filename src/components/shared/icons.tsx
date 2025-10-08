d55a8188134187565a17af7e2160e58bb3f0bc45
Mariano Ezequiel Gonzalez <marianoez.gonzalez@gmail.com>
2025-10-08 20:06:07 +0000

    Fijate que sigue estando el logo anterior, deberias usar el que te pase

--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -4,17 +4,20 @@ export function AhorroYaLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 256 256"
       width="1em"
       height="1em"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
-      <path fill="none" d="M0 0h256v256H0z" />
-      <path
-        fill="currentColor"
-        d="M216 64h-31.54a48.06 48.06 0 0 0-88.92 0H40a16 16 0 0 0-16 16v112a16 16 0 0 0 16 16h176a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16Zm-88-32a32 32 0 0 1 32 32h-64a32 32 0 0 1 32-32Zm88 160H40V80h56v16a8 8 0 0 0 8 8h64a8 8 0 0 0 8-8V80h56v144Z"
-      />
-      <circle fill="currentColor" cx="128" cy="140" r="12" />
+      <path d="M4 11V7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-4" />
+      <path d="M14 11h-4" />
+      <path d="M12 13V9" />
+      <path d="M20 11H8" />
     </svg>
   );
 }
