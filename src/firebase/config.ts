d6604cf9f11795cc632fbd946aa703c45843e6e8
Mariano Ezequiel Gonzalez <marianoez.gonzalez@gmail.com>
2025-10-08 20:21:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-211410928-89967",
+  "appId": "1:361417134147:web:a75ba8426bc05f165a04f6",
+  "apiKey": "AIzaSyCgaV8MHmFDuwQrvETWMkJkAXjN6X_9n3s",
+  "authDomain": "studio-211410928-89967.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "361417134147"
+};
