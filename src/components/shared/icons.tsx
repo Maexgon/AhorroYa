32455c6b7fab461bf1bc1a4a5700c246c8af5f86
Mariano Ezequiel Gonzalez <marianoez.gonzalez@gmail.com>
2025-10-21 01:18:53 +0000

    hay posibilidades de cambiar este logo de la pagina, el que esta al lado

--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -1,23 +1,19 @@
+import Image from "next/image";
 import type { SVGProps } from "react";
 
-export function AhorroYaLogo(props: SVGProps<SVGSVGElement>) {
+export function AhorroYaLogo(props: Omit<SVGProps<SVGSVGElement>, "width" | "height">) {
+  // Omit width and height from SVGProps to avoid conflict with Image component props
+  const { className, ...rest } = props;
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M4 11V7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-4" />
-      <path d="M14 11h-4" />
-      <path d="M12 13V9" />
-      <path d="M20 11H8" />
-    </svg>
+    <div className={className} style={{ position: 'relative' }}>
+      <Image
+        src="/img/logoAY.png"
+        alt="Ahorro Ya Logo"
+        fill
+        sizes="32px" // Provide a reasonable size hint
+        style={{ objectFit: 'contain' }}
+        {...rest}
+      />
+    </div>
   );
 }
