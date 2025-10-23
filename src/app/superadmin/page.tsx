524cb432c532b7de4dff355f15fff76cfc6a4274
Mariano Ezequiel Gonzalez <marianoez.gonzalez@gmail.com>
2025-10-23 13:27:24 +0000

    Try fixing this error: `Runtime Error: Error: CardDescription is not def

--- src/app/superadmin/page.tsx
+++ src/app/superadmin/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import React from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Users, Building, FileKey, Loader2, LogOut, DollarSign } from 'lucide-react';
 import { useUser, useFirestore, useMemoFirebase } from '@/firebase';
 import { useCollection } from '@/firebase/firestore/use-collection';
